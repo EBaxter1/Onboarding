7ea00ae25d3f28f93647981a80b2b5ee772d7f10
EBaxter1 <ebaxter1@hawk.iit.edu>
2017-02-01 16:42:21 -0600

    Added JavaScrpit that will move Submit button based on if email is correct to pattern

---
+++ js/site.js
@@ -0,0 +1,20 @@
+//Teel jQuery to give up the dollar sign 
+$.noConflict();
+
+//jQuery 3.x-style ready event and locally scoped $
+jQuery(function($){
+	$('html').removeClass('nojs');
+	$('html').addClass('hasjs');
+
+	$('#email').on('keyup focus blur',function(){
+		var currentValue = $(this).val();
+		var vaildPattern = $(this).attr('pattern');
+		console.log(currentValue,vaildPattern);
+		if(currentValue.match(vaildPattern)){
+			$('#submit').addClass('active');
+		}
+		else{
+			$('#submit').removeClass('active');
+		}
+	});
+});
